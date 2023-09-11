f3327611ffec76979d53cbbd1abeaa6bfd450c05
Cascoopman <cascoopman@hotmail.com>
2023-09-11 17:21:30 +0200

    Added the first .js file, a hashing  javascript

---
+++ hash.js
@@ -0,0 +1,22 @@
+const { createHash } = require('crypto');
+
+// create a string hash
+
+function hash(input) {
+    return createHash('sha256').update(input).digest('hex');
+
+}
+
+// compare two hashed passwords
+
+let password = 'hi-mom!';
+const hash1 = hash(password);
+console.log(hash1)
+
+// ... some time later
+
+password ='hi-mom';
+const hash2 = hash(passwords);
+const match = hash1 == hash2;
+
+console.log(match ? 'good password' : 'passwords do not match');
